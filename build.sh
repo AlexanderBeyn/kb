c197dacf4ee1a86af65fb8ba3218c389f6c3a28c
Alexander Beyn <malex@fatelectrons.org>
2020-05-25 18:36:17 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+NAME=kb
+DEST=./dist
+PLATFORMS="darwin/amd64 linux/amd64 windows/amd64"
+
+for platform in $PLATFORMS; do
+  export GOOS="${platform%/*}"
+  export GOARCH="${platform#*/}"
+  echo "Building for ${GOOS}/${GOARCH}..."
+  out="$DEST/${NAME}_${GOOS}_${GOARCH}_$(git describe --always --dirty)"
+  post="gzip -v $out"
+  if [ "$GOOS" = "windows" ]; then
+    out="${out}.exe"
+    post="zip --move ${out%.exe}.zip ${out}"
+  fi
+  go build -o "$out"
+  $post
+done
